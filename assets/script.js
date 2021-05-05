449db4cb5aeb9517df4c11e081279876ce4ac266
Ajurgs <ajjurgs5954@gmail.com>
2021-05-05 09:29:26 -0600

    added the change to search results page with parameters functionality

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
 const searchModal = document.querySelector("#searchModal");
+const searchFormat = document.querySelector("#format");
+const searchQuery = document.querySelector("#searchQuery");
 
 searchModal.addEventListener("submit", function (event) {
-  event.preventDefault;
+  event.preventDefault();
+  let format = searchFormat.value;
+  let query = searchQuery.value;
+
+  location.assign(`./searchresult.html?q=${query}&format=${format}`);
 });
