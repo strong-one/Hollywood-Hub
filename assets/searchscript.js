63d77022049562cba97d072b873a8e94ed1e6760
Ajurgs <ajjurgs5954@gmail.com>
2021-05-05 14:06:11 -0600

    added the show albums function

--- assets/searchscript.js
+++ assets/searchscript.js
@@ -115,8 +115,46 @@ function displayBand(data) {
           <h5 class="card-title">${band.strArtist}</h5>
           <p class="card-text">${band.strBiographyEN}</p>
           <h6 class="card-subtitle">discography</h6>
+          <div class="row" id="discography">
+            
+          </div>
         </div>
       </div>
   `;
   infoDisplay.innerHTML = temp;
+  getDiscography(band.idArtist);
+}
+
+function getDiscography(artistID) {
+  let url = `https://theaudiodb.com/api/v1/json/${aduiodbKey}/album.php?i=${artistID}`;
+  fetch(url)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      } else {
+        return response.json();
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      let albums = data.album;
+      let albumDisplay = document.querySelector("#discography");
+      albums.forEach((album) => {
+        let newCard = document.createElement("div");
+        newCard.classList.add("card");
+        newCard.setAttribute("style", "width: 18rem;");
+        newCard.innerHTML = `
+          <img class="card-img-top" src="${album.strAlbumThumb}" alt="Card image cap">
+          <div class="card-body">
+          <h5 class="card-title">${album.strAlbum}</h5>
+          <p class="card-text">${album.intYearReleased}</p>
+          </div>
+        `;
+
+        albumDisplay.appendChild(newCard);
+        // album name
+        // album cover
+        // release year
+      });
+    });
 }
