f597683d06461beda00302be213e6143570c54ac
Ajurgs <ajjurgs5954@gmail.com>
2021-05-06 21:12:22 -0600

    added the remove pin functionality to the pins

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,11 @@ const searchQuery = document.querySelector("#searchQuery");
 const artistPin = document.querySelector("#pins-insert");
 const tvshowPin = document.querySelector("#pins-insert-tv");
 const moviePin = document.querySelector("#pins-insert-mov");
+const pinDisplay = document.querySelector("#pinDisplay");
+const confrimRemovePinBtn = document.querySelector("#confirmRemoveBtn");
+// ----------------
+// event listeners
+// ----------------
 
 searchModal.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -12,6 +17,30 @@ searchModal.addEventListener("submit", function (event) {
 
   location.assign(`./searchresult.html?q=${query}&format=${format}`);
 });
+// pin level remove pin button click event
+pinDisplay.addEventListener("click", (event) => {
+  // target only the buttons in the cards
+  if (event.target.getAttribute("type") === "button") {
+    event.preventDefault();
+    console.log(event.target.getAttribute("pinName"));
+    // send the name of the pin to the modal remove button
+    confrimRemovePinBtn.setAttribute(
+      "pinName",
+      event.target.getAttribute("pinName")
+    );
+  }
+});
+// confirm remove pin modal button
+confrimRemovePinBtn.addEventListener("click", (event) => {
+  // remove the pin from the local storage array
+  removePin(confrimRemovePinBtn.getAttribute("pinName"));
+  // display updated list of pins
+  renderPins();
+});
+
+// -----------
+//  Functions
+// -----------
 
 // need to render filteredPins to populate on HTML
 
@@ -19,9 +48,11 @@ const renderPins = () => {
   //get the pins
   const pins = JSON.parse(localStorage.getItem("pins")) || [];
 
-  //create template - empty string to be able to display multiple templets for each pin
-  // let template = "";
-
+  // clear out the pin areas;
+  artistPin.innerHTML = "";
+  tvshowPin.innerHTML = "";
+  moviePin.innerHTML = "";
+  // generate the new content form the array of pins
   pins.forEach((pin) => {
     let newCard = document.createElement("div");
     newCard.classList.add("card");
@@ -35,6 +66,7 @@ const renderPins = () => {
         class="btn btn-danger btn-sm" 
         data-bs-toggle="modal"
         data-bs-target="#openConfirmRemoveModal"
+        pinName = "${pin.name}"
         id="openConfirmModalBtn"
       >
         Remove pin
